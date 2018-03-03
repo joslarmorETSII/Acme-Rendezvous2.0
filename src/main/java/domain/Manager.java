c53892194bc6829eb143f3a7ca6a43d8057a6aa5
khawla <alasfarkhawla@gmail.com>
2018-03-03 18:28:04 +0100

    Dominio Manager

---
+++ src/main/java/domain/Manager.java
@@ -0,0 +1,54 @@
+package domain;
+
+import org.hibernate.validator.constraints.NotBlank;
+
+import javax.persistence.*;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import java.util.Collection;
+
+@Entity
+@Access(AccessType.PROPERTY)
+public class Manager extends Actor {
+
+    // Constructors -----------------------------------------------------------
+
+    public Manager() {
+        super();
+    }
+
+
+    // Attributes -------------------------------------------------------------
+
+    private  String vat;
+
+    public String getVat() {
+        return vat;
+    }
+
+    @Column(unique = true)
+    @NotBlank
+    @Pattern(regexp = "^(\\d{3})\\-([A-Z]{3})$")
+    public void setVat(String vat) {
+        this.vat = vat;
+    }
+    // Relationships ----------------------------------------------------------
+
+    private Collection<Service> services;
+
+    @Valid
+    @NotNull
+    @OneToMany(mappedBy = "manager")
+    public Collection<Service> getServices() {
+        return services;
+    }
+
+    public void setServices(Collection<Service> services) {
+        this.services = services;
+    }
+
+
+
+
+}
