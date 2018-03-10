0b8c83bedb58c2c3243611ce3950950440f37136
khawla <alasfarkhawla@gmail.com>
2018-03-11 00:49:49 +0100

    Test Rendezvous

--- src/test/java/services/RendezvousServiceTest.java
+++ src/test/java/services/RendezvousServiceTest.java
@@ -77,8 +77,52 @@ public class RendezvousServiceTest extends AbstractTest {
             result.setForAdults(res3);
 
 
-            this.rendezvousService.save(result);
+           Rendezvous r= this.rendezvousService.save(result);
+            r = result;
+            this.unauthenticate();
 
+        } catch (final Throwable oops) {
+
+            caught = oops.getClass();
+
+        }
+
+        this.checkExceptions(expected, caught);
+    }
+
+    /*
+     * Edit a new rendezvous.
+     *
+     * En este caso de uso un usuario puede editar un rendezvous existente.
+     */
+
+
+    public void editRendezvousTest(final String username, final String name,String description1,String moment1,String picture1,Double lat1,Double lngi1,Boolean res1,Boolean res2,Boolean res3,String rendezvousBean, final Class<?> expected) {
+        Class<?> caught = null;
+
+        try {
+
+            this.authenticate(username);
+
+            Rendezvous result= rendezvousService.findOne(super.getEntityId(rendezvousBean));
+
+
+
+            result.setName(name);
+            result.setDescription(description1);
+
+            DateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+            result.setMoment(formatter.parse(moment1));
+            result.setPicture(picture1);
+            result.getLocation().setLatitude(lat1);
+            result.getLocation().setLongitude(lngi1);
+            result.setFinalMode(res1);
+            result.setDeleted(res2);
+            result.setForAdults(res3);
+
+
+            Rendezvous r = this.rendezvousService.save(result);
+            r = result;
             this.unauthenticate();
 
         } catch (final Throwable oops) {
@@ -93,7 +137,7 @@ public class RendezvousServiceTest extends AbstractTest {
     //Drivers
     // ===================================================
 
-    @Test
+    //@Test
     public void driverListRendezvousTest() {
 
         final Object testingData[][] = {
@@ -118,31 +162,67 @@ public class RendezvousServiceTest extends AbstractTest {
         for (int i = 0; i < testingData.length; i++)
             this.listOfRendezvousTest((String) testingData[i][0], (Class<?>) testingData[i][1]);
     }
-    @Test
+   // @Test
     public void driverRendezvousCreateTest() {
         //DateFormat formatter= new SimpleDateFormat("dd/MM/yyyy HH:mm");
 
         final Object testingData[][] = {
-                // Crear un curso estando logueado como academy -> true
-                {
-                        "user1", "name1","description1","12/03/2020 12:00","www.picture.com",20.99,13.09,false,false,false, null
+                // Crear un rendezvous estando logueado como user -> true
+    {
+        "user1", "name1","description1","12/03/2020 12:00","www.picture.com",20.99,13.09,false,false,false, null
+    },
+    // Crear un rendezvous sin estar logueado --> false
+    {
+        null,"name1","description1","12/03/2020 12:00","www.picture.com",20.99,13.09,false,false,false, IllegalArgumentException.class
+    },
+    // Crear un curso logueado como manager  -> false
+    {
+        "manager1","name1","description1","12/03/2020 12:00","www.picture.com",20.99,13.09,false,false,false, IllegalArgumentException.class
+    },
+    // Crear un curso rendezvous con momento en el pasado -> false
+    {
+        "manager1","name1","description1","12/03/2012 12:00","www.picture.com",20.99,13.09,false,false,false, IllegalArgumentException.class
+    },
+
+};
+        for (int i = 0; i < testingData.length; i++)
+        this.rendezvousCreateTest((String) testingData[i][0], (String) testingData[i][1],(String) testingData[i][2],(String)testingData[i][3] , (String)testingData[i][4],(Double) testingData[i][5],(Double) testingData[i][6],(Boolean) testingData[i][7],(Boolean) testingData[i][8],(Boolean) testingData[i][9], (Class<?>) testingData[i][10]);
+
+        }
+
+
+
+    @Test
+    public void driverEditRendezvousTest() {
+
+        final Object testingData[][] = {
+                // Crear un rendezvous estando logueado como user -> true
+        /*       {
+                        "user1", "name1","description1","12/03/2020 12:00","www.picture.com",20.99,13.09,false,false,false,"rendezvous1", null
+               },
+                // Crear un rendezvous sin estar logueado --> false
+               {
+                        null,"name1","description1","12/03/2020 12:00","www.picture.com",20.99,13.09,false,false,false,"rendezvous1", IllegalArgumentException.class
                 },
-                // Crear un curso sin estar logueado --> false
-                {
-                        null,"name1","description1","12/03/2020 12:00","www.picture.com",20.99,13.09,false,false,false, IllegalArgumentException.class
+                // editar el modo final y pasarlo a true -> true
+               {
+                        "user1","name1","description1","12/03/2020 12:00","www.picture.com",20.99,13.09,true,false,false,"rendezvous1", null
                 },
-                // Crear un curso logueado como dancer -> false
+                // Crear un curso logueado como manager  -> false
                 {
-                        "manager1","name1","description1","12/03/2020 12:00","www.picture.com",20.99,13.09,false,false,false, IllegalArgumentException.class
-                },
-                // Crear un curso rendezvous con momento en el pasado -> false
+                        "manager1","name1","description1","12/03/2020 12:00","www.picture.com",20.99,13.09,true,false,false, "rendezvous1",IllegalArgumentException.class
+               },*/
+                // Edit la description con un script -> false // todo: Preguntar al profesor, no se camptura el error de validacion
                 {
-                        "manager1","name1","description1","12/03/2012 12:00","www.picture.com",20.99,13.09,false,false,false, IllegalArgumentException.class
-                },
+                "user1",null,"<script>","12/03/2020 12:00","www.picture.com",20.99,13.09,true,false,false,"rendezvous1", IllegalArgumentException.class
+            }
+
 
         };
         for (int i = 0; i < testingData.length; i++)
-            this.rendezvousCreateTest((String) testingData[i][0], (String) testingData[i][1],(String) testingData[i][2],(String)testingData[i][3] , (String)testingData[i][4],(Double) testingData[i][5],(Double) testingData[i][6],(Boolean) testingData[i][7],(Boolean) testingData[i][8],(Boolean) testingData[i][9], (Class<?>) testingData[i][10]);
+            this.editRendezvousTest((String) testingData[i][0], (String) testingData[i][1],(String) testingData[i][2],(String)testingData[i][3] , (String)testingData[i][4],(Double) testingData[i][5],(Double) testingData[i][6],(Boolean) testingData[i][7],(Boolean) testingData[i][8],(Boolean) testingData[i][9],(String) testingData[i][10], (Class<?>) testingData[i][11]);
 
     }
+
+
 }
